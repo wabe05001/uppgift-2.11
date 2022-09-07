bf4caa824b5b0d768d7446fc5fec10976a55e89d
EmilWallett <nhlpool27@gmail.com>
2022-09-07 12:11:34 +0200

    Add project files.

---
+++ uppgift 2.11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Uppgift_2_11_
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("hur långt hoppade alma?");
+            string längdText = Console.ReadLine();
+            double längd = double.Parse(längdText);
+            double skillnad = 4.45 - längd;
+
+            Console.WriteLine("Elin hoppade" + 4.45 + "meterhögre");
+
+
+        }
+
+    }
+    
+
+
+    
+
+}
